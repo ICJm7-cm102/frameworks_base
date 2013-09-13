a1e931383d3bba1aed184e17eddcaecd1fcf7051
maxwen <max.weninger@gmail.com>
2013-09-13 23:40:50 +0200

    suspend actions: added mobile data toggle (1/2)
    Change-Id: I78ca67f92a3a27239ef0b4dfb4a7139b5af02e77

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3064,6 +3064,11 @@ public final class Settings {
          * @hide
          */
         public static final String SCREEN_STATE_GPS = "screen_state_gps";
+
+        /**
+         * @hide
+         */
+        public static final String SCREEN_STATE_MOBILE_DATA = "screen_state_mobile_data";
         
          /**
          * Show the NavBar dialog in Power menu
